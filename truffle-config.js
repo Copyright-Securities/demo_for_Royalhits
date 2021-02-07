59c5f6b48a7c3986c9125a93ef5715a511b54a09
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-02-07 16:38:02 +0100

    Rinkeby network added

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,25 @@
 require("babel-register");
 require("babel-polyfill");
 
+const { alchemyApiKey, mnemonic } = require("./secrets.json");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: 1337, // Match any network id
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`
+        ),
+      network_id: 4,
+      gasPrice: 10e9,
+      skipDryRun: true,
     },
   },
   contracts_directory: "./src/contracts/",
